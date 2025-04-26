0f0027fddeb1763359e55add3c0bd0036bcdfad5
pranto mollick <prantocse01@gmail.com>
2025-04-27 03:20:25 +0600

    Edit user page is done with Form as well

--- src/components/EditUser.tsx
+++ src/components/EditUser.tsx
@@ -8,17 +8,159 @@ import {
     SheetTrigger,
 } from "@/components/ui/sheet";
 import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "./ui/form";
+
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 const formSchema = z.object({
-    username: z.string().min(2).max(50),
+    username: z
+        .string()
+        .min(2, { message: "Username must be at least 2 characters" })
+        .max(50),
+    email: z.string().email({ message: "Invalid email address" }),
+    phone: z.string().min(10).max(15),
+    location: z.string().min(2),
+    role: z.enum(["admin", "user"]),
 });
 
 const EditUser = () => {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "john.doe",
+            email: "john.doe@gmail.com",
+            phone: "+1 234 5678",
+            location: "New York",
+            role: "admin",
+        },
+    });
     return (
         <SheetContent>
             <SheetHeader>
                 <SheetTitle className="mb-4">Edit User</SheetTitle>
-                <SheetDescription></SheetDescription>
+                <SheetDescription asChild>
+                    <div>
+                        <Form {...form}>
+                            <form className="space-y-8">
+                                <FormField
+                                    control={form.control}
+                                    name="username"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Username</FormLabel>
+                                            <FormControl>
+                                                <Input {...field} />
+                                            </FormControl>
+                                            <FormDescription>
+                                                This is your public display
+                                                name.
+                                            </FormDescription>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Email</FormLabel>
+                                            <FormControl>
+                                                <Input {...field} />
+                                            </FormControl>
+                                            <FormDescription>
+                                                Only admin can see your email.
+                                            </FormDescription>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="phone"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Phone</FormLabel>
+                                            <FormControl>
+                                                <Input {...field} />
+                                            </FormControl>
+                                            <FormDescription>
+                                                Only admin can see your phone
+                                                number.
+                                            </FormDescription>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="location"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Location</FormLabel>
+                                            <FormControl>
+                                                <Input {...field} />
+                                            </FormControl>
+                                            <FormDescription>
+                                                This is the public Location
+                                            </FormDescription>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="role"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Location</FormLabel>
+                                            <FormControl>
+                                                <Select>
+                                                    <SelectTrigger className="w-full">
+                                                        <SelectValue placeholder="Role" />
+                                                    </SelectTrigger>
+                                                    <SelectContent>
+                                                        <SelectItem value="admin">
+                                                            Admin
+                                                        </SelectItem>
+                                                        <SelectItem value="user">
+                                                            User
+                                                        </SelectItem>
+                                                    </SelectContent>
+                                                </Select>
+                                            </FormControl>
+                                            <FormDescription>
+                                                Only verifed users can be admin.
+                                            </FormDescription>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <Button type="submit">Submit</Button>
+                            </form>
+                        </Form>
+                    </div>
+                </SheetDescription>
             </SheetHeader>
         </SheetContent>
     );
